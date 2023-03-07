2df81220297c4043a80acd45889762ffd7223cfb
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-03-06 21:54:07 -0600

    feat(css & html files): adding missing pages & solving navigation issues

--- scripts/SingIn.js
+++ scripts/SingIn.js
@@ -4,12 +4,15 @@ const modal = document.getElementById("myModal");
 // Get the button that opens the modal
 const btn = document.getElementById("myBtn");
 
+
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close__modal")[0];
+
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+  modal.style.zIndex = "4";
 }
 
 // When the user clicks on <span> (x), close the modal
